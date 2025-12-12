f85fde619fe9e651dbe83ef77717551cb1feee8b
Mauro-Web-Pixel <maurogadiel72@gmail.com>
2025-12-12 23:31:25 +0000

    adiciona scripts `link:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Project.
- i don't know what to do and what to take it in the future with this. Lmao
+
+i don't know what to do and what to take it in the future with this. Lmao
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-    return <h1>Como vão todos vocês, pessoas e pessoxs. Esse é meu site, eu acho. Eu sou orfão.</h1>
+  return (
+    <h1>
+      Como vão todos vocês, pessoas e pessoxs. Esse é meu site, eu acho. Eu sou
+      orfão.
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
